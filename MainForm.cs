f85560488962e0f2a0d386b2d58a22ab2dc0b94d
Răzvan Trufin <144681144+razvantrufin99xx@users.noreply.github.com>
2024-05-24 10:34:50 +0300

    Add files via upload
    interesting math to draw a snail

---
+++ MainForm.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 5/24/2024
+ * Time: 10:09 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace melcDraw
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// T O DO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		public Graphics g;
+		
+		public Pen p = new Pen(Color.Black,1);
+		public Brush b = new SolidBrush(Color.Black);
+		
+		
+		public void drawMelc()
+		{
+			 int L = 200;
+			 int T = 200;
+			 float rx = 300;
+			 float ry = 300;
+			 double rad = 180/Math.PI;
+			 
+			 double  x = L*(Math.Cos(0/rad))+rx;
+			 double  y = T*(Math.Sin(0/rad))+ry;
+			 
+			 double px = x;
+			 double py = y;
+			 float iT = 0.0f;
+			 float step = 10.0f;
+			 float cerc =  360.0f;
+			 float z = 1/10;
+			 for(float i = 0.0f ; i<10*cerc+step; i+=step)
+			 {
+			 	iT += 1.0f;
+			 	z = (i/10);
+			 	x = (z+L)*(Math.Cos(i/rad))+rx+iT-z;
+			 	y = (z+T)*(Math.Sin(i/rad))+ry+iT-z;
+			 	
+			 	g.DrawLine(p,(float)x,(float)y,(float)px,(float)py);
+			 	
+			 	px = x;
+			 	py = y;
+			 }
+			 
+		
+		}
+		
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			g = this.CreateGraphics();
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			drawMelc();
+		}
+	}
+}
