c3fce578dbaf42672ceb70172a4a9c83d90b2dc9
Răzvan Trufin <144681144+razvantrufin99xx@users.noreply.github.com>
2024-05-24 22:45:35 +0300

    Add files via upload

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -15,6 +15,7 @@ namespace melcDraw
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel1;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -38,11 +39,12 @@ namespace melcDraw
 		private void InitializeComponent()
 		{
 			this.button1 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(1116, 56);
+			this.button1.Location = new System.Drawing.Point(12, 12);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 0;
@@ -50,11 +52,19 @@ namespace melcDraw
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.Button1Click);
 			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(94, 32);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1372, 740);
+			this.panel1.TabIndex = 1;
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1742, 973);
+			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.button1);
 			this.Name = "MainForm";
 			this.Text = "melcDraw";
--- MainForm.cs
+++ MainForm.cs
@@ -33,11 +33,10 @@ namespace melcDraw
 		
 		public Graphics g;
 		
-		public Pen p = new Pen(Color.Black,1);
+		public Pen p = new Pen(Color.FromArgb(255,0,0,0),1);
 		public Brush b = new SolidBrush(Color.Black);
 		
-		
-		public void drawMelc()
+		public void drawMelcOriginal()
 		{
 			 int L = 25;
 			 int T = 25;
@@ -72,10 +71,54 @@ namespace melcDraw
 		
 		}
 		
+		public void drawMelc()
+		{
+			 int L = 25;
+			 int T = 25;
+			 float rx = 300;
+			 float ry = 300;
+			 
+			 float posx = 200;
+			 float posy = 200;
+			 
+			 
+			 double rad = 180/Math.PI;
+			 
+			 double  x = L*(Math.Cos(0/rad))+rx;
+			 double  y = T*(Math.Sin(0/rad))+ry;
+			 
+			 double px = x;
+			 double py = y;
+			 float iT = 0.0f;
+			 float err = 2.0f;
+			 float step = err;
+			 float cerc =  360.0f;
+			 float z = 1/err;
+			 float handicap = 1.0f;
+			 float counter = 10;
+			 float zoom = 0.2500f;
+			 for(float i = 0.0f+handicap ; i<counter*cerc+step; i+=step)
+			 {
+			 	iT += 1.00f;
+			 	
+			 	z = (i/err);
+			 	x = (z+L)*(Math.Cos(i/rad))+rx+iT-z;
+			 	y = (z+T)*(Math.Sin(i/rad))+ry+iT-z;
+			 	posx += iT-z;
+			 	posy += iT-z;
+			 	g.DrawLine(p,(float)x*zoom+posx,(float)y*zoom+posy,(float)px*zoom+posx,(float)py*zoom+posy);
+			 	g.DrawEllipse(p,(float)x*zoom+posx,(float)y*zoom+posy,iT*zoom,iT*zoom);
+			 	px = x;
+			 	py = y;
+			 }
+			 
+		
+		}
+		
 		
 		void MainFormLoad(object sender, EventArgs e)
 		{
-			g = this.CreateGraphics();
+			g = this.panel1.CreateGraphics();
 		}
 		void Button1Click(object sender, EventArgs e)
 		{
